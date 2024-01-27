df7b945476727287ec110a869de4718a03c8836f
Bruno Staub <bruno.staub@gmail.com>
2024-01-27 21:29:34 +0100

    Update app.py
    Update comment

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Bruno. Container was Build with Jenkins CI/CD Pipeline!'
+    return 'Hello, everyone2!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
